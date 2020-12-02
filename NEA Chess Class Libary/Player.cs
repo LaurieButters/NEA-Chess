a060815eb5c214a3a2f96a1cec1a032a72edf246
Laurie Butterfield <196581@esher.ac.uk>
2020-12-02 15:39:18 +0000

    added player class

---
+++ NEA Chess Class Libary/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEA_Chess_Class_Libary
+{
+    class Player
+    {
+        private bool isWhite;
+        private bool isComputerPlayer;
+        public bool IsChecked()
+        {
+
+        }
+        public void InitializePieces()
+        {
+
+        }
+    }
+}
