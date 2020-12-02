adab17de22180de7ae9e5e9a03dd10a8175771bf
Laurie Butterfield <196581@esher.ac.uk>
2020-12-02 15:41:22 +0000

    added game class

---
+++ NEA Chess Class Libary/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEA_Chess_Class_Libary
+{
+    class Game
+    {
+        private Player currentPlayer;
+        private Player blackPlayer;
+        private Player whitePlayer;
+        private Board board;
+
+        public bool IsStalemate()
+        {
+
+        }
+        public bool IsCheckmate()
+        {
+
+        }
+    }
+}
